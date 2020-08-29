e500f3e1d584dc425562859e927e65e1275886e6
mnao305 <mail@mnao305.com>
2020-08-30 06:01:14 +0900

    add 配列シャッフル

---
+++ sort/shuffle.ts
@@ -0,0 +1,12 @@
+/**
+ * 配列をシャッフルする
+ * @param {number[]} ary - シャッフルする配列
+ * @returns {number[]} シャッフルした配列
+ */
+export const shuffle = (ary: number[]):number[] => {
+  for (let i = ary.length - 1; i >= 0; i--) {
+    const rand = Math.floor(Math.random() * i);
+    [ary[i], ary[rand]] = [ary[rand], ary[i]];
+  }
+  return ary;
+}
